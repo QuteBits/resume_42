11d23bf4db1b22b02e28b7bc4c8ef8d0ae550322
QuteBits <aleksandr.mattal@gmail.com>
2014-11-02 02:45:45 +0100

    Minor Stuff
    - comments added
    - small improvements here and there

--- resume_tex.py
+++ resume_tex.py
@@ -1,6 +1,6 @@
 # Converts my Resume from YAML to TeX.
 # Just don't forget to drop pdflatex on the output :)
-#
+# ------------------------------------------------------------------------------
 # @contributor Aleksandr Mattal <https://github.com/qutebits>
 # inspired by work of Brandon Amos <https://github.com/bamos/cv>
 
@@ -11,7 +11,7 @@ import sys
 from datetime import date
 from jinja2 import Environment, FileSystemLoader
 
-yaml_contents = yaml.load(open("resume.yaml", 'r'))
+yaml_contents = yaml.load(open("resume.yaml", 'r')) #read data
 
 env = Environment(loader=FileSystemLoader("template"),
   block_start_string='~{',block_end_string='}~',
@@ -21,22 +21,22 @@ this_loc = len(open("resume_tex.py", 'r').readlines()) #lets keep it at 42
 
 def generate():
   body = ""
-  for section in yaml_contents['order']:
+  for section in yaml_contents['order']: #generate sections 1 by 1
     contents = yaml_contents[section[0]]
     name = section[1].title()
     body += env.get_template("resume-section.tmpl.tex").render(
       name = name.upper(),
       contents = contents
     )
-
+  #and then generate the TeX wrapper and fill it with generated sections
   result = open("result/resume.tex", 'w')
   result.write(env.get_template("resume.tmpl.tex").render(
     name = yaml_contents['name'].upper(),
     email = yaml_contents['email'],
-    loc = this_loc,
+    loc = this_loc, #lines of code in this very script :)
     body = body,
-    today = date.today().strftime("%B %d, %Y")
+    today = date.today().strftime("%B %d, %Y") #generation date
   ))
   result.close()
 
-generate()
+generate() #finally, generate this beauty
